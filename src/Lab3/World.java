d6fd96b330547f8ca90cc33fe5e010485ce4c84b
apred <apred@10.6.26.203>
2024-12-11 22:19:08 -0500

    game

--- src/Lab3/World.java
+++ src/Lab3/World.java
@@ -2,18 +2,18 @@ package Lab3;
 
 public class World {
 public static Room buildWorld() {
-	Room room1 = new Room("You are in room 1.");
-	Room room2= new Room("you are in room 2.") ;
-	Room room3= new Room("you are in room 3.") ;
-	Room room4= new Room("you are in room 4.") ;
-	Room room5= new Room("you are in room 5.") ;
-	Room room6= new Room("you are in room 6 .") ;
+	Room room1 = new Room("You are in room 1. There are two doors on your right and a flashlight on the floor;");
+	Room room2= new Room("you are in room 2. There is a puppy in the middle of the room and a door infront of you." ) ;
+	Room room3= new Room("you are in room 3. There is a desk  with a key sitting on top of it. and a hatch above and below you" ) ;
+	Room room4= new Room("you are in room 4. there is a map folded in the corner of the room there is also only one door infront of you and a hatch above you") ;
+	Room room5= new Room("you are in room 5. there is a door on your right and a goblin blocking the door on your right") ;
+	Room room6= new Room("you are in room 6. there are no doors and windows but just a safe sitting in the middle of the room") ;
 	
 	room1.addExit('e',room2);
-	room2.addExit('w',room3);
+	room2.addExit('u',room3);
 	room3.addExit('n',room4);
 	room4.addExit('s',room5);
-	room5.addExit('u',room6);
+	room5.addExit('w',room6);
 	room6.addExit('d',room1);
 	
 	Item a =  new Item("desk","you can block off a room with this");
@@ -24,14 +24,16 @@ public static Room buildWorld() {
 	Item f = new Item("teleporter","you automaticaly travel to room5");
 	Item g = new Item("diamond","siny valuable rock");
 	NPC h = new NPC("Puppy","a puppy wags his tail");
+	NPC i = new NPC("goblin","a goblin blocks you path");
 	room1.setItem("Pencil",c);
 	room1.setItem("Phone",b);
-	room1.setItem("desk", a);
+	room3.setItem("desk", a);
 	room1.setItem("Map", d);
 	room2.setNPC("Puppy",h);
+	room5.setNPC("goblin", i);
 	
 	Safe safe = new Safe("safe","I wonder whats inside");
-	room1.setItem("safe", safe);
+	room6.setItem("safe", safe);
 	Combination combo = new Combination("safe combo", "12345");
 	
 	room1.setlock(true);
--- src/Lab3/goblin.java
+++ src/Lab3/goblin.java
@@ -4,5 +4,42 @@ public class goblin extends NPC {
 	public goblin() {
 		super("goblin","An angry looking goblin blocks your path");
 	}
-	
+	int a = 0;
+	public void talk() {
+		switch(a) {
+		case 1:
+		say("You can't go through this door because I said so");
+		String[] options = {
+				"Why not",
+				"please can I go through"
+		};
+		getResponse(options);
+		case 2:
+		say("I'm not letting you through this door");
+		String[] options2 = {
+			"well if your going to be mean Im just going to move you",
+			"Alright your annoying im just going through the other door."
+		};
+		getResponse(options2);
+		}
+	}
+	public void response(int option) {
+		switch(option) {
+		case 1:
+			say("becase I said so you got a hearing problem");
+			break;
+		case 2:
+			say("Ok beacuse you asked nicely ill let you go through");
+			Game.print("The golbin moved you can now go through the door in you would like");	
+			break;
+		case 3:
+			say("No you can't move me I'm a goblin");
+			Game.print("you and the golbin get in a fight and you move the goblin");
+			break;
+		case 4:
+			say("Wait, wait, wait why don't you want to go through my door.");
+		}
+		
+		
+	}
 }
